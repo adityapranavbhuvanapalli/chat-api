1e64b0c3a32c57f4aa8ead58cb136dce31601489
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2022-12-09 19:47:37 +0530

    security with jwt token auth

--- src/main/java/com/chat/chatapi/ChatApiApplication.java
+++ src/main/java/com/chat/chatapi/ChatApiApplication.java
@@ -1,8 +1,11 @@
 package com.chat.chatapi;
 
+import com.chat.chatapi.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class ChatApiApplication {
 
